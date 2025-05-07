e2980093fe55d816ab344834b441522e37f8353f
alexsu <alexsu@alexsus-MacBook-Air.local>
2025-05-07 16:34:54 -0700

    Fixes for tests

--- src/core/controller/index.ts
+++ src/core/controller/index.ts
@@ -542,7 +542,7 @@ export class Controller {
 				const settingsFilter = message.text || ""
 				await vscode.commands.executeCommand(
 					"workbench.action.openSettings",
-					`@ext:saoudrizwan.claude-dev ${settingsFilter}`.trim(), // trim whitespace if no settings filter
+					`@ext:aiwize.wizee ${settingsFilter}`.trim(), // trim whitespace if no settings filter
 				)
 				break
 			}
